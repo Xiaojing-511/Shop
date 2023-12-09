24ddcb2e0f1f3f2a677673c09d0bcdf05f8e14be
Rachel Zhang <rachelzhang@hokdo.com>
2023-12-09 23:17:47 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// import React from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(11);
